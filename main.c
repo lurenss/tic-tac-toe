6716c397f300f6cf2f2fcc7dd55f045bdf00f895
Lorenzo Padoan <lorenzo.padoan977@gmail.com>
2019-02-07 17:53:19 +0100

    create graphic

--- main.c
+++ main.c
@@ -3,12 +3,32 @@
 
 
 void graphic(){
-	for(int i=0;i<;i++){
-		for(int j=0;j<;j++){
-			if(i%2!=1){
 
+	char game_camp [3][7]; 
+	int i,j;
+	/*for(j=0;j<3;j++){
+		for(i=0;i<7;i++){
+			game_camp[j][i]='0';
 		}
+	}	*/	
+
+	for(j=0;j<3;j++){
+		for(i=0;i<7;i++)
+			if(i%2==0)
+				game_camp[j][i]='|';
+			else
+				game_camp[j][i]=' ';
+	}
+	for(j=0;j<3;j++){
+		for(i=0;i<7;i++){
+			printf("%c",game_camp[j][i]);
+		}
+		printf("\n");
+	}		
+
+
 }
 int main(){
+	graphic();
 	return 0;
 }
