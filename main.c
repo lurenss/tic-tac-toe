30052bb9e6c11e1515232d5f50fdbdb23d184bf6
Lorenzo Padoan <lorenzo.padoan977@gmail.com>
2019-02-08 19:40:53 +0100

    inizialize play function

--- main.c
+++ main.c
@@ -1,34 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
-void graphic(){
+char* init(char* game_camp){
 
-	char game_camp [3][7]; 
-	int i,j;
-	/*for(j=0;j<3;j++){
-		for(i=0;i<7;i++){
-			game_camp[j][i]='0';
-		}
-	}	*/	
-
-	for(j=0;j<3;j++){
-		for(i=0;i<7;i++)
-			if(i%2==0)
-				game_camp[j][i]='|';
+	for(int i=0;i<21;i++){
+		if((i>=0 && i<=6) || (i>=14 &&i<=20)) {	
+			if(i%2==0)		
+				*(game_camp+i)='|';
 			else
-				game_camp[j][i]=' ';
-	}
-	for(j=0;j<3;j++){
-		for(i=0;i<7;i++){
-			printf("%c",game_camp[j][i]);
+				*(game_camp+i)=' ';
 		}
-		printf("\n");
-	}		
-
+		else{
+			if(i%2!=0)		
+				*(game_camp+i)='|';
+			else
+				*(game_camp+i)=' ';	
+		}	
+	}
+	return game_camp;		
+}	
+	
+void draw(char* game_camp){
+	for(int i=0;i<21;i++){
+		printf("%c",*(game_camp+i));
+		if(i==6||i==13||i==20)
+			printf("\n");
+	}
 
 }
+
+void play(char* game_camp){
+	char buffer[256];
+	int x
+	memset(buffer,' ',255);
+	while(gamseisEnd()){
+		draw(game_camp);
+		printf("Insert coordinate\n");
+		scanf(%s,buffer);
+		control = sscanf(buffer,"%d",&x);
+		if(control<2)
+			printf("Coordinate error: insert a nuber from 1 to 9\n");
+		switch
+	}
+}
+	
+
 int main(){
-	graphic();
+	char* game_camp;
+	game_camp = (char*)malloc(sizeof(char)*3*7);
+	init(game_camp);
+	draw(game_camp);
 	return 0;
 }
