dfe56b0914c88575c8526a9c986579bf2c7a00e6
Lorenzo Padoan <lorenzo.padoan977@gmail.com>
2019-02-10 18:46:07 +0100

    add ia

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 
 char* init(char* game_camp){
@@ -31,6 +32,101 @@ void draw(char* game_camp){
 
 }
 
+void ia(char* game_camp){
+	int x;
+	int flag=1;
+	srand(time(NULL));
+	do{
+		x=rand()%9;
+		switch(x){
+			case 1:
+				if(*(game_camp+1)!=' '){
+					break;		
+				}
+				else{
+					*(game_camp+1)='o';
+					flag=0;
+					break;
+				}
+			case 2:
+				if(*(game_camp+3)!=' '){
+					break;
+				}	
+				else{
+					*(game_camp+3)='o';
+					flag=0;
+					break;
+				}	
+			case 3:
+				if(*(game_camp+5)!=' '){
+					break;
+				}	
+				else{
+					*(game_camp+5)='o';
+					flag=0;
+					break;
+				}	
+			case 4:
+				if(*(game_camp+8)!=' '){
+					break;
+				}	
+				else{
+					*(game_camp+8)='o';
+					flag=0;
+					break;
+				}	
+			case 5:
+				if(*(game_camp+10)!=' '){
+					break;
+				}	
+				else{
+					*(game_camp+10)='o';
+					flag=0;
+					break;
+				}	
+			case 6:
+				if(*(game_camp+12)!=' '){
+					break;
+				}	
+				else{
+					*(game_camp+12)='o';
+					flag=0;
+					break;
+				}	
+			case 7:
+				if(*(game_camp+15)!=' '){
+					break;
+				}	
+				else{
+					*(game_camp+15)='o';
+					flag=0;
+					break;
+				}	
+			case 8:
+				if(*(game_camp+17)!=' '){
+					break;
+				}	
+				else{
+					*(game_camp+17)='o';
+					flag=0;
+					break;
+				}	
+			case 9:
+				if(*(game_camp+19)!=' '){
+					break;
+				}	
+				else{
+					*(game_camp+19)='o';
+					flag=0;
+					break;
+				}
+			default:
+            	printf("Coordinate error: insert a number from 1 to 9\n");	
+		}
+	}
+	while(flag);	
+
+}
 void player_move(char* game_camp){
 	char buffer[256];
 	int x,control;
@@ -51,6 +147,7 @@ void player_move(char* game_camp){
 				}	
 				else{
 					*(game_camp+1)='x';
+					ia(game_camp);
 					break;
 				}
 			case 2:
@@ -60,6 +157,7 @@ void player_move(char* game_camp){
 				}	
 				else{
 					*(game_camp+3)='x';
+					ia(game_camp);
 					break;
 				}	
 			case 3:
@@ -69,6 +167,7 @@ void player_move(char* game_camp){
 				}	
 				else{
 					*(game_camp+5)='x';
+					ia(game_camp);
 					break;
 				}	
 			case 4:
@@ -78,6 +177,7 @@ void player_move(char* game_camp){
 				}	
 				else{
 					*(game_camp+8)='x';
+					ia(game_camp);
 					break;
 				}	
 			case 5:
@@ -87,6 +187,7 @@ void player_move(char* game_camp){
 				}	
 				else{
 					*(game_camp+10)='x';
+					ia(game_camp);
 					break;
 				}	
 			case 6:
@@ -96,6 +197,7 @@ void player_move(char* game_camp){
 				}	
 				else{
 					*(game_camp+12)='x';
+					ia(game_camp);
 					break;
 				}	
 			case 7:
@@ -105,6 +207,7 @@ void player_move(char* game_camp){
 				}	
 				else{
 					*(game_camp+15)='x';
+					ia(game_camp);
 					break;
 				}	
 			case 8:
@@ -114,6 +217,7 @@ void player_move(char* game_camp){
 				}	
 				else{
 					*(game_camp+17)='x';
+					ia(game_camp);
 					break;
 				}	
 			case 9:
@@ -123,6 +227,7 @@ void player_move(char* game_camp){
 				}	
 				else{
 					*(game_camp+19)='x';
+					ia(game_camp);
 					break;
 				}
 			default:
